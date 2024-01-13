e43e983196be57161451a4e4e42f42a43422eea9
bikash-pal <bpal13577@gmail.com>
2024-01-13 14:03:19 +0530

    creating delete and post api call

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -16,10 +16,10 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return Ok(VilllaStore.villaList);
+            return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -29,9 +29,9 @@ namespace MagicVilla_VillaAPI.Controllers
             {
                 return BadRequest();
             }
-            var villa = VilllaStore.villaList.FirstOrDefault(u => u.Id == id);
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
 
-            if(villa == null)
+            if (villa == null)
             {
                 return NotFound();
             }
@@ -39,24 +39,75 @@ namespace MagicVilla_VillaAPI.Controllers
         }
 
         [HttpPost]
-        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO) 
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already exists");
+                return BadRequest(ModelState);
+            }
             if (villaDTO == null)
             {
                 return BadRequest();
             }
-            else if (villaDTO.Id > 0) 
+            else if (villaDTO.Id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
-            villaDTO.Id= VilllaStore.villaList.Max(u => u.Id)+1;
+            villaDTO.Id = VillaStore.villaList.Max(u => u.Id) + 1;
 
-            VilllaStore.villaList.Add(villaDTO);
-            
-            return Ok(villaDTO);
+            VillaStore.villaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { Id = villaDTO.Id }, villaDTO);
 
         }
+
+        [HttpDelete("{id:int}", Name ="DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if(villa == null) 
+            {
+                return NotFound();
+            }
+
+            VillaStore.villaList.Remove(villa);
+
+            return NoContent();
+
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO) 
+        { 
+            if (villaDTO == null || id != villaDTO.Id) 
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            villa.Name = villaDTO.Name;
+            villa.SqFt = villaDTO.SqFt;
+            villa.Occupancy = villaDTO.Occupancy;
+
+            return NoContent();
+        }
+
     }
 }
 
--- MagicVilla_VillaAPI/Store/VillaStore.cs
+++ MagicVilla_VillaAPI/Store/VillaStore.cs
@@ -2,12 +2,12 @@
 
 namespace MagicVilla_VillaAPI.Store
 {
-    public class VilllaStore
+    public class VillaStore
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>()
             {
-                new VillaDTO() {Id=1,Name="Bikash"},
-                new VillaDTO() {Id=2,Name="Aikash"}
+                new VillaDTO() {Id=1,Name="Bikash",Occupancy =1,SqFt=234},
+                new VillaDTO() {Id=2,Name="Aikash",Occupancy =13,SqFt=2334}
             };
     }
 }
