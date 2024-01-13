679402e8c57f03e902d252201393af6dfc104550
bikash-pal <bpal13577@gmail.com>
2024-01-13 11:02:30 +0530

    adding first controller and get action method

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,39 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Store;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VilllaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VilllaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+    }
+}
+
---
+++ MagicVilla_VillaAPI/Store/VilllaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Store
+{
+    public class VilllaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>()
+            {
+                new VillaDTO() {Id=1,Name="Bikash"},
+                new VillaDTO() {Id=2,Name="Aikash"}
+            };
+    }
+}
